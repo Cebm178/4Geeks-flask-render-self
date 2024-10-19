7d5098b19131d154d346b11f49697d3af2c83258
Cebm178 <cebm178@yahoo.com>
2024-10-19 00:09:09 +0000

    Third Submission

--- src/app.py
+++ src/app.py
@@ -72,5 +72,8 @@ def predict():
     
     return render_template('index.html', predicted_price=predicted_price, error=error)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    # Use the port provided by Render, or default to 5000
+    port = int(os.environ.get("PORT", 5000))
+    # Set host to 0.0.0.0 to be accessible externally
+    app.run(host="0.0.0.0", port=port, debug=True)
